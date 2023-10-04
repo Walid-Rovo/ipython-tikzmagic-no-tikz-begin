786b9c80cfc64b1378f9dfeb21fe4f827dc1fdcb
Walid Ismail <walid.ismail@micropsi-industries.com>
2023-10-04 14:55:46 +0300

    Update `setup.py` to version `0.2.0`

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
-from __future__ import with_statement
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 classifiers = [
-    "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: BSD License",
     "Topic :: Scientific/Engineering :: Visualization",
     "Topic :: Software Development :: Libraries",
     "Topic :: Utilities",
-    "Framework :: IPython"
+    "Framework :: IPython",
 ]
 
 with open("README.md", "r") as fp:
     long_description = fp.read()
 
-__author__ = "Michael Kraus"
-__version__ = "0.1.1"
+__author__ = "Walid Ismail (Original Author: Michael Kraus)"
+__version__ = "0.2.0"
 
 setup(
     name="ipython-tikzmagic",
     version=__version__,
     author=__author__,
-    url="https://github.com/mkrphys/ipython-tikzmagic",
+    url="https://github.com/Walid-Rovo/ipython-tikzmagic-no-tikz-begin",
     py_modules=["tikzmagic"],
-    description="IPython magics for generating figures with TikZ",
+    description="IPython magics for generating figures with TikZ without an implicit inclusion"
+                "of `\begin{tikzpicture[...]}`, which allows greater flexibility of the latex "
+                "code (e.g. using `\tikzset` before `\begin{tikzpicture}`)",
     long_description=long_description,
     license="BSD",
     classifiers=classifiers,
     install_requires=[
         "ipython",
-    ]
+        "Pillow",
+        "PyMuPDF",
+    ],
+    python_requires=">=3.8",
 )
